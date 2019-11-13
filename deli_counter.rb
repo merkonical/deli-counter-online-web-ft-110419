2efa22bc7c4c034273cf3bb6863cfc8cfef2ea9f
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 06:35:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(deli)
     puts "The line is currently empty."
   else 
     puts "The line is currently:"
-    
+    deli.each.with_index do |i, c|
+      puts "line is currently: #{i}. #{c}"
   end 
 end 
