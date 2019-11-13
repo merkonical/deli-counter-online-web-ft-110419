ea262a62e714d4e1c0c1651217fe73761a2b3367
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 06:20:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,8 @@ require "pry"
 def line(deli) 
   if deli.empty?
     puts "The line is currently empty."
-  binding.pry
+binding.pry
   else 
-    
     displays current place in line 
-   
   end 
 end 
