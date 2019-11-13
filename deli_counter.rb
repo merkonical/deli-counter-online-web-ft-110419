6c538870b32ab4eb778ebfe2d90832fcf4fc0770
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 07:26:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(deli, person)
   if deli.empty?
     deli << person 
   end  
-  deli.each.with_index do |person, i|
+  deli.each.with_index(1) do |person, i|
     puts "Welcome, #{person}. You are number #{i} in line."
   end
 end 
