82291ee7a27c564f4c4066e8929fd634b3449006
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 06:49:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli)
   else 
     line_number = "The line is currently:" #turned into variable to add numbers and names in phrase
     deli.each.with_index(1) do |c, i|
-      line_number << " #{i}. #{c}"
+      line_number << " #{i}.#{c}"
     end 
     puts line_number
     binding.pry 
