15dfc67bdb9032c668d2e089b3f078abeadcd357
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 06:50:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,11 @@ def line(deli)
   if deli.empty?
     puts "The line is currently empty."
   else 
-    line_number = "The line is currently:" #turned into variable to add numbers and names in phrase
+    line_number = "The line is currently:" 
     deli.each.with_index(1) do |c, i|
       line_number << " #{i}.#{c}"
     end 
     puts line_number
-    binding.pry 
+     
   end  
 end 
