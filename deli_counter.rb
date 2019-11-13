3b32a8d0b3b27fd50d58039f249950e1447e9c6c
Maverick Adolfo <merkonical@gmail.com>
2019-11-13 06:04:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
+require 'pry'
 
 def line(katz_deli) 
   if line is == 0 
     puts "The line is currently empty."
+    binding.pry 
   else line is > 0 
     displays current line 
 end 
